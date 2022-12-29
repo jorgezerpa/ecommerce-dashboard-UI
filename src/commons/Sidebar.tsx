7681cfb3bee5cff2330f0a9b8137b544ca751604
jorgezerpa <jorgezerpacoder@gmail.com>
2022-12-29 14:10:55 -0400

    add tag to products fetch api

--- src/commons/Sidebar.tsx
+++ src/commons/Sidebar.tsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import { useRouter } from 'next/router'
 
+const items = [
+    { title:'products', path:'/products' },
+    { title:'categories', path:'/categories' },
+    { title:'profile', path:'/profile' },
+]
+
 const Sidebar = () => {
     const router = useRouter()
 
     return (
         <div className='h-screen w-[200px] bg-gray-300'>
             <ul>
-                <li onClick={()=>router.push('/products')}>Products</li>
-                <li onClick={()=>router.push('/categories')}>Categories</li>
-                <li onClick={()=>router.push('/profile')}>Profile</li>
+                { items.map((item, index)=>(
+                    <>
+                        <div className='bg-gray-200 h-[3px] w-full'></div>
+                        <li className='font-bold text-white text-lg bg-gray-600 p-3' key={`sidebarMenu${index}`} onClick={()=>router.push(item.path)}>{ item.title }</li>
+                    </>
+                )) }
             </ul>
         </div>
     )
--- src/services/coreApi/products.ts
+++ src/services/coreApi/products.ts
@@ -3,6 +3,7 @@ import { RootState } from '../../store/store'
 
 export const productsApi = createApi({
     reducerPath:'productsApi', 
+    tagTypes:['products'],
     baseQuery: fetchBaseQuery({
         baseUrl: 'https://commerce-api.zerpacode.com/api/v1/dashboard',
         prepareHeaders: (headers, store) => {
@@ -10,11 +11,11 @@ export const productsApi = createApi({
         },
     }),
     endpoints: (build) => ({
-        getProducts: build.query<any, void>({query:() => ({ url: `product`})}),
-        getProduct: build.query<any, string>({query:(productId) => ({ url: `product/${productId}`})}),
-        createProduct: build.mutation<any, void>({query:(data) => ({ url: `product`, method:'POST', body:data })}),
-        updateProduct: build.mutation<any, {productId:string, data:any}>({query:({productId, data}) => ({ url: `product/${productId}`, method:'PATCH', body:data })}),
-        deleteProduct: build.mutation<any, string>({query:(productId) => ({ url: `product/${productId}`, method:'DELETE' })}),
+        getProducts: build.query<any, void>({query:() => ({ url: `product`}), providesTags:['products']}),
+        getProduct: build.query<any, string>({query:(productId) => ({ url: `product/${productId}`}), providesTags:['products']}),
+        createProduct: build.mutation<any, void>({query:(data) => ({ url: `product`, method:'POST', body:data }), invalidatesTags:['products']}),
+        updateProduct: build.mutation<any, {productId:string, data:any}>({query:({productId, data}) => ({ url: `product/${productId}`, method:'PATCH', body:data }), invalidatesTags:['products']}),
+        deleteProduct: build.mutation<any, string>({query:(productId) => ({ url: `product/${productId}`, method:'DELETE' }), invalidatesTags:['products']}),
     }),
 })
 
