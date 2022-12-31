4cb16b9f237bf04fd61a89cc43cb802a9a85a490
jorgezerpa <jorgezerpacoder@gmail.com>
2022-12-31 11:08:36 -0400

    incorporate refresh token sistem

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,15 +1,35 @@
 import React, { PropsWithChildren, useEffect } from 'react'
+import Cookies from 'js-cookie'
 import { useRouter } from 'next/router'
 import { useAppSelector } from '../hooks/rtkHooks'
+import { useRefreshMutation } from 'services/coreApi/auth'
+import { useAppDispatch } from '../hooks/rtkHooks'
+import { login, logout } from '../store/authSlices/authSlice'
 import Sidebar from 'commons/Sidebar'
 import TopBar from 'commons/TopBar'
 
 export const MainLayout = ({children}:PropsWithChildren) => {
   const router = useRouter()
+
+  const dispatch = useAppDispatch()
   const isLoggedIn = useAppSelector(state=>state.authSlice.isLoggedIn)
+  const [refresh] = useRefreshMutation()
 
   useEffect(()=>{
-    if(!isLoggedIn) router.push('/login')
+    if(isLoggedIn){
+      let interval = setInterval(()=>{
+            if(Cookies.get('refreshToken')){
+              refresh({ refreshToken: Cookies.get('refreshToken') as string })
+                .unwrap()
+                .then(data=>dispatch(login({ token:data.data.token, refreshToken:data.data.refreshToken })))        
+            } else {
+              dispatch(logout())
+              router.push('/login')
+            }
+      }, (1000 * 60 * 5)-20) 
+      } else{
+        router.push('/login')
+      } 
   },[isLoggedIn])
 
   return (
--- src/services/coreApi/auth.ts
+++ src/services/coreApi/auth.ts
@@ -4,10 +4,13 @@ export const authApi = createApi({
     reducerPath:'authApi',
     baseQuery: fetchBaseQuery({baseUrl: 'https://commerce-api.zerpacode.com/api/v1/'}),
     endpoints: (build) => ({
-        login: build.mutation({
-            query: (data) => ({ url: `dashboard/auth/login`, method: 'POST', body: data, }),
-        }),
+        login: build.mutation({ query: (data) => ({ url: `dashboard/auth/login`, method: 'POST', body: data, }) }),
+        refresh: build.mutation({ query: (data:{refreshToken:string}) => ({ 
+            url: `dashboard/auth/refresh`, 
+            method: 'POST', 
+            body: data,
+        }) }),
     }),
 })
 
-export const { useLoginMutation } = authApi
+export const { useLoginMutation, useRefreshMutation } = authApi
